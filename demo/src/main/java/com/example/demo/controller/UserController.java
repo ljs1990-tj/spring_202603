03f99beaf33f5ce569a77fbbecf37fdb52180b97
test <ljs1990.tj@gmail.com>
2026-04-08 15:20:54 +0900

    2026.04.08

--- demo/src/main/java/com/example/demo/controller/UserController.java
+++ demo/src/main/java/com/example/demo/controller/UserController.java
@@ -1,19 +1,29 @@
 package com.example.demo.controller;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
 
 import com.example.demo.dao.UserService;
 import com.google.gson.Gson;
 
 import jakarta.servlet.http.HttpServletRequest;
+
 
 @Controller
 public class UserController {
@@ -21,8 +31,16 @@ public class UserController {
 	@Autowired
 	UserService userService;
 	
+	@Value("${client_id}")
+	private String client_id;
+
+    @Value("${redirect_uri}")
+    private String redirect_uri;
+	
 	@RequestMapping("/login.do") 
 	public String login(Model model) throws Exception{
+		String location = "https://kauth.kakao.com/oauth/authorize?response_type=code&client_id="+client_id+"&redirect_uri="+redirect_uri;
+        model.addAttribute("location", location);
 		return "/user/login";
 	}
 	
@@ -90,5 +108,37 @@ public class UserController {
 
 		return new Gson().toJson(resultMap); 
 	}
+	
+	@RequestMapping("/main.do") 
+	public String main(Model model) throws Exception{
+		return "/main";
+	}
+	
+	@RequestMapping(value = "/kakao.dox", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
+	@ResponseBody
+	public String kakao(Model model, @RequestParam HashMap<String, Object> map) throws Exception {
+		HashMap<String, Object> resultMap = new HashMap<String, Object>();
+		String tokenUrl = "https://kauth.kakao.com/oauth/token";
+
+        RestTemplate restTemplate = new RestTemplate();
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", "authorization_code");
+        params.add("client_id", client_id);
+        params.add("redirect_uri", redirect_uri);
+        params.add("code", (String) map.get("code"));
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(params, headers);
+        ResponseEntity<Map> response = restTemplate.postForEntity(tokenUrl, request, Map.class);
+
+        Map<String, Object> responseBody = response.getBody();
+        System.out.println(responseBody);
+
+		return new Gson().toJson(resultMap); 
+	}
+	
+	
 	
 }
